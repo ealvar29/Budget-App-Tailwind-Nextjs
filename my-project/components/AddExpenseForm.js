c893c0fde975fe5ffa60562a280b2e404fb567f6
Eduardo <ealvarez@carminaticonsulting.com>
2021-07-02 17:15:53 -0500

    added ability to add expenses

--- my-project/components/AddExpenseForm.js
+++ my-project/components/AddExpenseForm.js
@@ -1,6 +1,28 @@
+import { useState, useContext } from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
 const AddExpenseForm = () => {
+  const { dispatch } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [cost, setCost] = useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+    setName("");
+    setCost("");
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <div className="grid grid-cols-2">
         <div className="text-center">
           <label for="name">Name</label>
@@ -11,6 +33,8 @@ const AddExpenseForm = () => {
             type="text"
             className="border border-red-600 form-control"
             id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           ></input>
         </div>
         <div className="text-center">
@@ -22,6 +46,8 @@ const AddExpenseForm = () => {
             type="text"
             className="border border-red-600 form-control"
             id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
           ></input>
         </div>
         <div className="text-center">
